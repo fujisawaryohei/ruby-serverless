2d8382c811e681994ffb682eaeb8564f8c26077f
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-11-03 23:27:42 +0900

    dynamoDBのキースキーマの変更とLambdaとDynamoDBのgetItem処理を追加

--- handler.rb
+++ handler.rb
@@ -1,3 +1,4 @@
+require "bundler/setup"
 require 'json'
 require 'aws-sdk'
 
@@ -14,8 +15,18 @@ end
 def get_todo(event:, context:)
   dynamoDB = Aws::DynamoDB::Resource.new(region: 'ap-northeast-1')
   table = dynamoDB.table('my-vue-calendar-db')
-  response = table.get_item({
-    key: { 'timestamp': '2020-10-1'}
-  })
-  response.to_json
+  params = {
+    table_name: 'my-vue-calendar-db',
+    key: {
+      timestamp: '2020-10-1'
+    }
+  }
+  response = table.get_item(params)
+  {
+    statusCode: 200,
+    body: {
+      timestamp: response.item['timestamp'],
+      content: response.item['todo']
+    }.to_json
+  }
 end
