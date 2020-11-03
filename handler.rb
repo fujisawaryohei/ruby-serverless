b7ddbe55d7406aebff2d66dd31be816114a45e44
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-11-03 14:04:33 +0900

    一旦コミット

--- handler.rb
+++ handler.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'aws-sdk'
 
 def hello(event:, context:)
   {
@@ -8,4 +9,13 @@ def hello(event:, context:)
       input: event
     }.to_json
   }
+end
+
+def get_todo(event:, context:)
+  dynamoDB = Aws::DynamoDB::Resource.new(region: 'ap-northeast-1')
+  table = dynamoDB.table('my-vue-calendar-db')
+  response = table.get_item({
+    key: { 'timestamp': '2020-10-1'}
+  })
+  response.to_json
 end
